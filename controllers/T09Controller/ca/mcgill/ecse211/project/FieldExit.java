8f1c74e61d5cbf4a8f44bb8f774ca303da9ba795
Antonia Nistor <antonianistor@hotmail.fr>
2020-11-23 14:47:35 -0500

    start FieldExit

---
+++ controllers/T09Controller/ca/mcgill/ecse211/project/FieldExit.java
@@ -0,0 +1,39 @@
+package ca.mcgill.ecse211.project;
+
+import static ca.mcgill.ecse211.project.Resources.TIMEOUT_PERIOD;
+import ca.mcgill.ecse211.playingfield.Point;
+import simlejos.hardware.ev3.LocalEV3;
+
+
+public class FieldExit {
+    
+/** Values retrieved from server */
+static Point Island_LL = Resources.island.ll;
+static Point Island_UR = Resources.island.ur;
+
+static Point Z_LL;
+static Point Z_UR;
+static Point TN_LL;
+static Point TN_UR;
+static Point TN_UL;
+static Point TN_LR;
+static Point SZ_LL;
+static Point SZ_UR;
+
+/**
+   * Main method that performs the exit of field
+   */
+  public static void exitField() {
+
+
+    // 1. travel to tunnel
+
+    //2. cross tunnel
+
+    //3. go to start corner
+
+
+
+  }
+
+}
