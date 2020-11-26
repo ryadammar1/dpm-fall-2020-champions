c8930aa94993698d1c8d55e3ced1cf00908c8be9
Alban Moreon <alban@MacBook-Pro-de-Alban.local>
2020-11-25 20:25:34 -0500

    Added first measurements discarding for sensor2

--- controllers/T09Controller/ca/mcgill/ecse211/project/Avoidance.java
+++ controllers/T09Controller/ca/mcgill/ecse211/project/Avoidance.java
@@ -96,6 +96,9 @@ public class Avoidance {
 			return filter((int) (usData[0] * 100.0));
 		}
 		if (usId == 2) {
+			for (int i = 0; i < 3; i++) {
+				usSensor2.fetchSample(usData, 0);
+			}
 			usSensor2.fetchSample(usData, 0);
 			return filter((int) (usData[0] * 100.0));
 		}
